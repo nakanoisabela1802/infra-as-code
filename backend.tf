2fa1df42f5e5e301ee807e8d54069a2218d6a0d0
nakanoisabela1802 <nakanoisabela163@gmail.com>
2026-03-09 19:22:06 -0300

    add tf files

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# state.tf
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-lab-devops-saori"
+    key     = "site/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+    use_lockfile = truue
+  }
+}
